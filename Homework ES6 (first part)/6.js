6568c7ad3e2cf3ddb8e85e37b8e110062f675567
YazhovaAlla <2977407@mail.ru>
2022-02-28 22:32:36 +0300

    Update 6.js

--- Homework ES6 (first part)/6.js
+++ Homework ES6 (first part)/6.js
@@ -2,14 +2,13 @@ function countVowelLetters(text) {
     text = text.toLowerCase().split('');
 
     const vowelLetters = ['а', 'я', 'ы', 'и', 'о', 'ё', 'у', 'ю', 'э', 'е', 'a', 'e', 'i', 'o', 'u', 'y'];
-       let counter = 0;
-
-    for (let value of text) {
-        if (vowelLetters.includes(value)) {
-            counter++;
+    let counter = 0;
+    text.forEach(item => {
+            if (vowelLetters.includes(item)) {
+                counter++;
+            }
         }
-    }
-
+    );
     return counter;
 }
 
